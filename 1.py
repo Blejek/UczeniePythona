1b64b4b732ee3a370ef0088002f6bd83e21a9f3f
Blejek <101128150+Blejek@users.noreply.github.com>
2022-03-10 18:09:11 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,10 +11,13 @@ przecinek = 3.14 #liczby zmiennoprzecinkowe
 
 
 #zamienianie wartości
+#zamiast 1 dać trzeba jakąś wartość albo zmienną
 
 str(1) #do stringu
 int(1) #do integeru/inta
 float(1) #do floatu
 bool(1) #do boolu/boolean
 
-print(str(integer))
+
+#drukuje zmienną 'integer' zamienioną na string + jakiś tekst
+print(str(integer) + " o to nasza liczba")
