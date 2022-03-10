26a0d07105c77dbb09442e785a97626511922301
Blejek <101128150+Blejek@users.noreply.github.com>
2022-03-10 18:20:46 +0100

    PodstawyPetliInstrukcjiWarunkowych

---
+++ entity.py
@@ -0,0 +1,53 @@
+
+#zmienne
+gra = True
+
+ile = 0
+
+liczba = 2
+
+
+#pętla while
+#dajemy warunek i do kiedy ten warunek jest spełniony będzie się wykonywać pętla
+while gra:
+    ile += 1
+
+    print(ile)
+
+    if ile == 10:
+        gra = False
+
+
+#przedzielenie
+print("")
+
+#pętla for
+
+#for i(to jest zmienna gdzie będzie przechowywa liczba powtórzeń) in range(ile razy ma się powtórzyć)/(argument pierwszy to od której liczby zaczynamy a ten drugi to liczba do której będzie się powtarzać)
+#pętla się skończy gdy i będzie równenadanemu warunkowi
+
+for i in range(90,121):
+    print(i)
+
+
+#instrukcje warunkowe
+
+
+#sprawdza jeśli nasza liczba == 2
+if liczba == 2:
+
+    print("liczba == 2")
+
+#elif/else wykonywane jest jeśli warunek u góry jest nie spełniony.
+#do elif można dać taki sam warunek jak do if
+
+#sprawdza jeśli nasza liczba > od 2
+elif liczba > 2:
+
+        print("liczba > od 2")
+
+
+#sprawdza jeśli nasza liczba < od 2
+elif liczba < 2:
+    
+        print("liczba < od 2")
